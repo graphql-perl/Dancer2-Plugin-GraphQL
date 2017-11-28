96342fd058762d7a978b240fe7854f4c1af7c4c7
Ed J <mohawk2@users.noreply.github.com>
2017-11-28 07:14:41 +0000

    v0.06

--- lib/Dancer2/Plugin/GraphQL.pm
+++ lib/Dancer2/Plugin/GraphQL.pm
@@ -7,7 +7,7 @@ use Dancer2::Plugin;
 use GraphQL::Execution qw(execute);
 use Module::Runtime qw(require_module);
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 has graphiql => (
   is => 'ro',
